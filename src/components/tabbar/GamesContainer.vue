e027e31388f2993adf6fead9d3bfa4a113cbfc9d
CptSwift <chenzihao000@qq.com>
2018-11-07 17:07:28 +0800

    更新主界面为卡片形式

--- src/components/tabbar/GamesContainer.vue
+++ src/components/tabbar/GamesContainer.vue
@@ -1,58 +1,49 @@
 <template>
-    <div>
-
-                <!-- 九宫格 到 6宫格 的改造工程 -->
-        <ul class="mui-table-view mui-grid-view mui-grid-9">
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
-                <router-link to="/games/jump">
-                <img src="../../images/menu1.png" alt="">
-                <div class="mui-media-body">跳一跳</div></router-link></li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
-                <router-link to="/home/photolist">
-                <img src="../../images/menu2.png" alt="">
-                <div class="mui-media-body">图片分享</div></router-link></li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
-                <router-link to="/home/goodlist">
-                <img src="../../images/menu3.png" alt="">
-                <div class="mui-media-body">商品购买</div></router-link></li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
-                <img src="../../images/menu4.png" alt="">
-                <div class="mui-media-body">留言反馈</div></li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
-                <img src="../../images/menu5.png" alt="">
-                <div class="mui-media-body">视频专区</div></li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-4">
-                <img src="../../images/menu6.png" alt="">
-                <div class="mui-media-body">联系我们</div></li>
-        </ul>
-
-    </div>
+	<div class="games_container">
+		<div class="mui-card" v-for="(item, i) in gamesList" :key="item.id">
+			<router-link tag="div" :to="{path: item.linker}">
+				<div class="mui-card-header"><b>{{ item.name }}</b></div>
+				<div class="mui-card-header mui-card-media" style="height:40vw; background-image:item.img_src"></div>
+				<div class="mui-card-content">
+					<div class="mui-card-content-inner">
+						<p>Posted on {{ item.time }}</p>
+						<p style="color: #333;">{{ item.des }}</p>
+					</div>
+				</div>
+			</router-link>
+		</div>
+	</div>
 </template>
 
 <script>
-    import { Toast } from "mint-ui";
+	import { Toast } from "mint-ui";
 
-    export default {
-
-    };
+	export default {
+		data() {
+			return {
+				gamesList: [{
+					id: 1,
+					time: '2018-1-1',
+					name: '跳一跳',
+					des: '模仿Chrome断网小恐龙',
+					linker: '/games/jump',
+					img_src: '../../images/dragon.jpg'
+				},{
+					id: 2,
+					time: '2018-1-1',
+					name: '黑白棋',
+					des: '风行文曲星学习机的上古游戏',
+					linker: '/games/jump',
+					img_src: '../../images/dragon.jpg'
+				}]
+			}
+		}
+	};
 </script>
 
 <style lang="scss" scoped>
-
-    .mui-grid-view.mui-grid-9 {
-        background-color: #fff;
-        border: none;
-	    img {
-	        width: 60px;
-	        height: 60px;
-	    }
-	
-	    .mui-media-body{
-	        font-size: 13px;
-	    }
-    }
-
-    .mui-grid-view.mui-grid-9 .mui-table-view-cell {
-        border: 0;
-    }
+	.games_container{
+		background-color: #eee;
+		overflow: hidden;
+	}
 </style>
